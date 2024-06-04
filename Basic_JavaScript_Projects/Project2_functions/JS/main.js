8352e84f7a467061a105647cc3f1cd0a6f2de7d4
Jose Hernandez <jahdezjr424@gmail.com>
2024-06-03 21:36:17 -0400

    My Second JavaScript Project
    My Basic JavaScript Project Assignment for The Tech Academy.

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I love to ";
+    sentence += " play Airsoft!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
