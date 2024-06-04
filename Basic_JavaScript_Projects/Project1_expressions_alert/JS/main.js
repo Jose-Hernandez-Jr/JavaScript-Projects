8555a757eafcc19e8faa3633bb3cd4763a56e71b
Jose Hernandez <jahdezjr424@gmail.com>
2024-06-03 21:07:27 -0400

    My First JavaScript  Project
    My Basic JavaScript Project Assignment for The Tech Academy.

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+// window.alert("Here we are.")
+
+// document.write("Hello World. ")
+
+// var A = "This is a string"
+// document.write(A);
+
+// var B = " I said \"Hello People!\""
+// document.write(B)
+
+// document.write("They said \" Lets get down with the sickness\""
+// + "-Disturbed");
+
+// var Airsoft = "Airsoft Weapons", AR = "M4", SUB = "MP5", LMG = "249 SAW", SNIPER = "SSG96";
+// document.write(SNIPER)
+
+// document.write(4 + 4); //Writing a Expression.
+
+window.alert("Basic JavaScript Projects Assignment.") //My Alert
+
+document.write("Here we go with this.") //My write
+
+var sent1 = "This is the beginning of the string" //My First String
+var sent2 = "and this is the end of the string" //My Second String
+
+document.write(sent1 + sent2 ); //My Concatenate Variable
+
+document.write(5 + 10); //My Expression
